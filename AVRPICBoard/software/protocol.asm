8bff43e94845c5bd5bb44932cdd555d170f0bafb
vrstanchev <vrstanchev@gmail.com>
2023-09-26 11:51:12 -0400

    software finished

--- AVRPICBoard/software/protocol.asm
+++ AVRPICBoard/software/protocol.asm
@@ -1,7 +1,7 @@
 ;data constants
 section .data
 STRT_BIT equ 1
-BTS equ 101
+INFO equ 101
 STPBIT equ 0
 ;code started
 section .text
@@ -11,14 +11,14 @@ mov eax,1
 mov ebx, 0
 ;compare for communication start
 cmp ebx, STRT_BIT
-je .communicate
+je communicate
 communicate:
-mov ecx,BTS
+mov ecx,INFO
 cmp ecx,0
-jne .finish
+jne finish
 finish:
 mov ebx,0
 cmp ebx,STPBIT
-mov ebx,1
+mov ebx,2
 ;system cals
 int 0x80
